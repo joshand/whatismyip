4a08078edb0abaddd717e2888f03de4567f644dd
Josh Anderson <joshand@cisco.com>
2017-09-19 13:24:27 -0500

    add v1 api

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, Response
+import json
 
 
 # -- Application Setup
@@ -26,6 +27,23 @@ def get_ip():
         xfw = ""
 
     return xfw
+
+
+# -- Add route to GET status for a task
+@app.route('/api/v1/whatismyip', methods=['GET'])
+def get_ip_v2():
+    print("GET /api/v1/whatismyip")
+
+    x_remaddr = request.remote_addr
+    x_env = request.environ
+    x_env["REMOTE_ADDR"] = x_remaddr
+    del x_env["wsgi.input"]
+    del x_env["wsgi.errors"]
+    del x_env["werkzeug.server.shutdown"]
+    del x_env["werkzeug.request"]
+    print(x_env)
+
+    return json.dumps(x_env)
 
 
 # -- Main function
